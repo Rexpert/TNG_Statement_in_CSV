dde59ad60e4568550b252ff5f549a92a793af1a4
Rexpert <46991185+Rexpert@users.noreply.github.com>
2022-12-21 20:18:13 +0800

    :fire: Remove unwanted code

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import pandas as pd
 import numpy as np
 import camelot
-# import matplotlib.pyplot as plt
-# from matplotlib.ticker import MultipleLocator
 
 table = camelot.read_pdf(r'.\data\tng_ewallet_transactions.pdf', pages='all', flavor='stream',
                         table_regions=['20,600,820,50'], columns=['80,140,230,294,460,660,720'], 
                         split_text=True, strip_text='\n')
-
-# fig, ax = plt.subplots()
-# pg = camelot.plot(test[0], kind='text')
-# yaxis = plt.gca().yaxis
-# yaxis.set_major_locator(MultipleLocator(10))
-# xaxis = plt.gca().xaxis
-# xaxis.set_major_locator(MultipleLocator(10))
-# xaxis.set_tick_params(labelrotation=90)
-# plt.grid(True)
-# pg.show()
 
 df = (
     pd
