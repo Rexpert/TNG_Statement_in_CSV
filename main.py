d33a933064b3198839ddcdad58034691abe11767
Rexpert <46991185+Rexpert@users.noreply.github.com>
2023-12-07 11:58:56 +0800

    :pencil2: Fix typo on wrong columns sep

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import camelot
 # Read PDF Statement into a table collection, the areas/regions and columns separators is self-defined
 table = camelot.read_pdf(r'.\data\tng_ewallet_transactions.pdf', pages='all', flavor='stream',
                         table_regions=['20,600,820,50'], table_areas=['20,600,820,50'], 
-                        columns=['80,140,230,2444,460,660,720'], 
+                        columns=['80,140,230,294,460,660,720'], 
                         split_text=True, strip_text='\n')
 
 # Merge all tables and clean the data
